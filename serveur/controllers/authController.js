c37797d417a0a9a5405ab47d117667ba65930aa9
aminata054 <tinaami054@gmail.com>
2025-03-11 10:57:30 +0000

    ajout

---
+++ serveur/controllers/authController.js
@@ -0,0 +1,62 @@
+const user = require('../models/user');
+const { hashPassword, comparePassword } = require('../helpers/auth')
+
+const test = (req, res) => {
+    res.json("Hello from auth controller");
+};
+
+const hashPassword = await hashPassword(password)
+
+
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const exist = await user.findOne({ email });
+        if (exist) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const user = await user.create({ name, email, password: hashPassword });
+        if (user) {
+            return res.status(201).json({ message: "User created successfully" });
+        }
+
+    } catch (error) {
+
+    }
+
+}
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await user.findOne({ email });
+        if (user) {
+            return res.json({
+                error: 'No user found'
+            })
+        }
+
+        const match = await comparePassword(password, user.password)
+        if (match) {
+            res.json('password match')
+        }
+
+    } catch (error) {
+
+    }
+}
+
+
+module.exports = {
+    test,
+    registerUser,
+    loginUser,
+
+};
