f4b19eb5f828c0a4d73d2698d47f2d52a087962c
aminata054 <tinaami054@gmail.com>
2025-03-12 11:38:00 +0000

    Adding gitignore

--- serveur/routes/topicRoutes.js
+++ serveur/routes/topicRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
+const { createTopic } = require("../controllers/topicController");
 
 // middleware
 router.use(
@@ -10,6 +11,6 @@ router.use(
     })
 );
 
-router.post("",)
+router.post("/createTopic", createTopic)
 
 module.exports = router
